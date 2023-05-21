829b5ffe5f44866cca0e64e733e14e034e40bdf1
Peng Wang <wp2204@gmail.com>
2023-05-21 10:07:13 +0000

    Update example Compartment_4Exits_2020
    Actually it is compartment with 2 Exits! Have fun!

--- examples/Compartment_4Exits_2020/run.bat
+++ examples/Compartment_4Exits_2020/run.bat
@@ -2,6 +2,6 @@
 
 Title Run Example for Windows
 
-python ../../code/code_version2.0/main.py tpre2020-2_Version1.0.csv
+python ../../code/code_version2.2/main.py tpre2020-2_Version1.0.csv TPRE_DET2.fds 
 
 pause
